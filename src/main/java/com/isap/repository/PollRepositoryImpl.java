5010641a16bb80d977799bc842156c55a586c254
Mohammed Isap <mohammed.isap@theverygroup.com>
2024-11-21 08:08:56 +0000

    Add logs to the Service and Repo layers to aid better in debugging and monitioring

--- src/main/java/com/isap/repository/PollRepositoryImpl.java
+++ src/main/java/com/isap/repository/PollRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.isap.domain.Poll;
 import com.isap.domain.Vote;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.*;
 
@@ -19,12 +20,11 @@ import static com.isap.domain.Option.SK_OPTION;
 import static com.isap.domain.Poll.SK_POLL;
 import static com.isap.domain.Vote.SK_VOTE;
 
-
 @ApplicationScoped
+@Slf4j
 public class PollRepositoryImpl implements PollRepository {
 
     private static final String TABLE_NAME = "PollTable";
-
     private final DynamoDbClient dynamoDbClient;
 
     @Inject
@@ -34,119 +34,179 @@ public class PollRepositoryImpl implements PollRepository {
 
     @Override
     public GetItemResponse getPollByPollId(String pollId) {
+        log.debug("Requesting Poll with ID: {}", pollId);
+
+        if (pollId == null || pollId.isEmpty()) {
+            log.warn("Poll ID is missing or empty");
+            return null;
+        }
+
         GetItemRequest request = GetItemRequest.builder()
                 .tableName(TABLE_NAME)
-                .key(Map.of(
-                        "PK", AttributeValue.builder().s(pollId).build(),
-                        "SK", AttributeValue.builder().s(SK_POLL).build()
-                ))
+                .key(Map.of("PK", AttributeValue.builder().s(pollId).build(),
+                        "SK", AttributeValue.builder().s(SK_POLL).build()))
                 .build();
 
-        return dynamoDbClient.getItem(request);
+        log.debug("GetItemRequest: {}", request);
+        GetItemResponse response = dynamoDbClient.getItem(request);
+
+        if (response == null || response.item().isEmpty()) {
+            log.warn("Poll not found for PollID: {}", pollId);
+        } else {
+            log.debug("Poll retrieved: {}", response.item());
+        }
+
+        return response;
     }
 
     @Override
     public boolean incrementVoteCount(String pollId, String optionId) {
+        log.debug("Incrementing vote count for PollID: {} and OptionID: {}", pollId, optionId);
+
         UpdateItemRequest updateItemRequest = UpdateItemRequest.builder()
-                .tableName(TABLE_NAME) // DynamoDB table name
-                .key(Map.of(
-                        "PK", AttributeValue.builder().s(optionId).build(),
-                        "SK", AttributeValue.builder().s(SK_OPTION).build()
-                ))
+                .tableName(TABLE_NAME)
+                .key(Map.of("PK", AttributeValue.builder().s(optionId).build(),
+                        "SK", AttributeValue.builder().s(SK_OPTION).build()))
                 .updateExpression("SET VoteCount = VoteCount + :increment")
-                .expressionAttributeValues(Map.of(
-                        ":increment", AttributeValue.builder().n("1").build()
-                ))
+                .expressionAttributeValues(Map.of(":increment", AttributeValue.builder().n("1").build()))
                 .returnValues(ReturnValue.UPDATED_NEW)
                 .build();
 
+        log.debug("UpdateItemRequest: {}", updateItemRequest);
         UpdateItemResponse updateItemResponse = dynamoDbClient.updateItem(updateItemRequest);
 
-        if (updateItemResponse.sdkHttpResponse().isSuccessful())
+        if (updateItemResponse.sdkHttpResponse().isSuccessful()) {
+            log.info("Vote count incremented successfully for OptionID: {}", optionId);
             return putVote(pollId, optionId);
+        } else {
+            log.error("Failed to increment vote count for OptionID: {}. Response: {}", optionId, updateItemResponse.sdkHttpResponse());
+        }
 
         return false;
     }
 
     private boolean putVote(String pollId, String optionId) {
-        // Insert the poll question into DynamoDB
+        log.debug("Inserting vote for PollID: {} and OptionID: {}", pollId, optionId);
+
         PutItemRequest votePutRequest = PutItemRequest.builder()
                 .tableName(TABLE_NAME)
-                .item(new Vote(UUID.randomUUID().toString(), pollId, optionId, Instant.now().toString())
-                        .toDynamoDbItem())
+                .item(new Vote(UUID.randomUUID().toString(), pollId, optionId, Instant.now().toString()).toDynamoDbItem())
                 .returnValues(ReturnValue.UPDATED_NEW)
                 .build();
 
         PutItemResponse putItemResponse = dynamoDbClient.putItem(votePutRequest);
 
-        return putItemResponse.sdkHttpResponse().isSuccessful();
+        if (putItemResponse.sdkHttpResponse().isSuccessful()) {
+            log.info("Vote inserted successfully for PollID: {} and OptionID: {}", pollId, optionId);
+            return true;
+        } else {
+            log.error("Failed to insert vote for PollID: {} and OptionID: {}. Response: {}", pollId, optionId, putItemResponse.sdkHttpResponse());
+        }
+
+        return false;
     }
 
     @Override
     public QueryResponse getVotesByPollId(String pollId) {
-        QueryRequest queryRequest = QueryRequest.builder()
-                .tableName(TABLE_NAME)
-                .indexName("GSI1")  // Using GSI1 for querying
-                .keyConditionExpression("GSI1PK = :pollId AND GSI1SK = :SK")
-                .expressionAttributeValues(Map.of(
-                        ":pollId", AttributeValue.builder().s(pollId).build(),
-                        ":SK", AttributeValue.builder().s(SK_VOTE).build()))
-                .build();
+        log.debug("Querying votes for PollID: {}", pollId);
 
-        return dynamoDbClient.query(queryRequest);
-    }
-
-    @Override
-    public QueryResponse getOptionsByPollId(String pollId) {
         QueryRequest queryRequest = QueryRequest.builder()
                 .tableName(TABLE_NAME)
                 .indexName("GSI1")
                 .keyConditionExpression("GSI1PK = :pollId AND GSI1SK = :SK")
-                .expressionAttributeValues(Map.of(
-                        ":pollId", AttributeValue.builder().s(pollId).build(),
+                .expressionAttributeValues(Map.of(":pollId", AttributeValue.builder().s(pollId).build(),
+                        ":SK", AttributeValue.builder().s(SK_VOTE).build()))
+                .build();
+
+        log.debug("QueryRequest: {}", queryRequest);
+        QueryResponse response = dynamoDbClient.query(queryRequest);
+
+        if (response.items().isEmpty()) {
+            log.warn("No votes found for PollID: {}", pollId);
+        } else {
+            log.debug("Votes retrieved for PollID: {}: {}", pollId, response.items());
+        }
+
+        return response;
+    }
+
+    @Override
+    public QueryResponse getOptionsByPollId(String pollId) {
+        log.debug("Querying options for PollID: {}", pollId);
+
+        QueryRequest queryRequest = QueryRequest.builder()
+                .tableName(TABLE_NAME)
+                .indexName("GSI1")
+                .keyConditionExpression("GSI1PK = :pollId AND GSI1SK = :SK")
+                .expressionAttributeValues(Map.of(":pollId", AttributeValue.builder().s(pollId).build(),
                         ":SK", AttributeValue.builder().s(SK_OPTION).build()))
                 .build();
 
-        return dynamoDbClient.query(queryRequest);
+        log.debug("QueryRequest: {}", queryRequest);
+        QueryResponse response = dynamoDbClient.query(queryRequest);
+
+        if (response.items().isEmpty()) {
+            log.warn("No options found for PollID: {}", pollId);
+        } else {
+            log.debug("Options retrieved for PollID: {}: {}", pollId, response.items());
+        }
+
+        return response;
     }
 
     @Override
     public boolean createPoll(Map<String, List<String>> newPollData) {
+        log.info("Creating a new poll with data: {}", newPollData);
+
         if (newPollData.size() != 1) {
+            log.error("Invalid poll data format. Expected exactly one entry, but got: {}", newPollData.size());
             throw new IllegalArgumentException("New poll data should have exactly one entry");
         }
 
-        // Extract poll question and options from the newPollData map
         String pollId = UUID.randomUUID().toString();
         String question = newPollData.keySet().iterator().next();
         List<String> options = newPollData.get(question);
 
-        // Create the map of UUIDs to options
+        log.debug("Poll Question: {}, Options: {}", question, options);
+
         Map<String, String> optionsMap = options.stream()
                 .collect(Collectors.toMap(option -> UUID.randomUUID().toString(), option -> option));
 
-        // Insert the poll question into DynamoDB
         PutItemRequest pollPutRequest = PutItemRequest.builder()
                 .tableName(TABLE_NAME)
-                .item(new Poll(pollId, question, optionsMap)
-                        .toDynamoDbItem())
+                .item(new Poll(pollId, question, optionsMap).toDynamoDbItem())
                 .returnValues(ReturnValue.UPDATED_NEW)
                 .build();
 
+        log.debug("Creating poll request: {}", pollPutRequest);
+
         PutItemResponse putPollItemResponse = dynamoDbClient.putItem(pollPutRequest);
+
+        if (!putPollItemResponse.sdkHttpResponse().isSuccessful()) {
+            log.error("Failed to create poll. Poll Question: {}", question);
+            return false;
+        }
 
         List<PutItemResponse> putOptionItemResponses = new ArrayList<>();
         optionsMap.forEach((key, value) -> {
             PutItemRequest optionPutRequest = PutItemRequest.builder()
                     .tableName(TABLE_NAME)
-                    .item(new Option(key, pollId, value, 0)
-                            .toDynamoDbItem())
+                    .item(new Option(key, pollId, value, 0).toDynamoDbItem())
                     .build();
 
+            log.debug("Creating Option with Id: {}, Value: {}", key, value);
             putOptionItemResponses.add(dynamoDbClient.putItem(optionPutRequest));
         });
 
-        return putPollItemResponse.sdkHttpResponse().isSuccessful() &&
-                (putOptionItemResponses.size() == optionsMap.size());
+        boolean allOptionsCreated = putOptionItemResponses.stream()
+                .allMatch(response -> response.sdkHttpResponse().isSuccessful());
+
+        if (allOptionsCreated) {
+            log.info("Poll and options created successfully for PollID: {}", pollId);
+        } else {
+            log.error("Failed to create all options for PollID: {}", pollId);
+        }
+
+        return putPollItemResponse.sdkHttpResponse().isSuccessful() && allOptionsCreated;
     }
 }
--- src/main/java/com/isap/service/PollServiceImpl.java
+++ src/main/java/com/isap/service/PollServiceImpl.java
@@ -17,8 +17,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static com.isap.domain.Poll.fromDynamoDbItem;
 import static com.isap.domain.Option.fromQueryResponse;
+import static com.isap.domain.Poll.fromDynamoDbItem;
 
 @Path("/poll")
 @Slf4j
@@ -36,30 +36,38 @@ public class PollServiceImpl implements PollService {
     @Produces(MediaType.APPLICATION_JSON)
     @Override
     public Response getPoll(@QueryParam("pollId") String pollId) {
+        log.info("Received request to get poll with pollId: {}", pollId);
+
         if (pollId == null || pollId.isEmpty()) {
+            log.warn("Poll ID is missing or empty");
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity("Poll ID is required").build();
         }
 
+        log.debug("Fetching poll data for pollId: {}", pollId);
         GetItemResponse response = pollRepository.getPollByPollId(pollId);
 
         if (response == null || response.item().isEmpty()) {
+            log.warn("Poll not found for pollId: {}", pollId);
             return Response.status(Response.Status.NOT_FOUND).entity("Poll not found").build();
         }
 
         Poll poll = fromDynamoDbItem(response.item());
+        log.info("Poll retrieved successfully: {}", poll);
 
         Map<String, Object> result = new HashMap<>();
         result.put("pollId", poll.pollId());
         result.put("question", poll.question());
 
         if (poll.options().size() > 7 || poll.options().size() < 2) {
+            log.error("Poll size out of bounds: {}", poll.options().size());
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity(String.format("Poll has an invalid number of options: %s", poll.options().size()))
                     .build();
         }
 
         result.put("options", poll.options());
+        log.debug("Poll options: {}", poll.options());
 
         return Response.ok(result).build();
     }
@@ -73,39 +81,50 @@ public class PollServiceImpl implements PollService {
         String optionId = requestBody.get("optionId");
 
         if (pollId == null || pollId.isEmpty()) {
+            log.warn("Poll ID is missing or empty");
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity("Poll ID is required").build();
         }
 
         if (optionId == null || optionId.isEmpty()) {
+            log.warn("Option ID is missing or empty");
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity("Option ID is required").build();
         }
 
+        log.info("Received vote request for pollId: {} and optionId: {}", pollId, optionId);
+
         try {
+            log.debug("Attempting to increment vote count for pollId: {} and optionId: {}", pollId, optionId);
             if (pollRepository.incrementVoteCount(pollId, optionId)) {
+                log.info("Vote count updated successfully for pollId: {} and optionId: {}", pollId, optionId);
                 return Response.ok(getOptionsByPollId(pollId)).build();
             } else {
-                log.error("Error while updating vote count in DynamoDB.");
+                log.error("Failed to update vote count in DynamoDB for pollId: {} and optionId: {}", pollId, optionId);
             }
         } catch (DynamoDbException e) {
-            log.error("Error while updating vote count in DynamoDB: {}", e.getMessage());
+            log.error("Error while updating vote count in DynamoDB: {}", e.getMessage(), e);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
                     .entity("Failed to update the vote count. Please try again").build();
         }
 
+        log.warn("Failed to update vote count for pollId: {} and optionId: {}", pollId, optionId);
         return Response.status(Response.Status.BAD_REQUEST)
                 .entity("Failed to update the vote count. Please try again")
                 .build();
     }
 
     private Response getOptionsByPollId(String pollId) {
+        log.debug("Fetching options with vote stat for pollId: {}", pollId);
+
         if (pollId == null || pollId.isEmpty()) {
+            log.warn("Poll ID is missing or empty");
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity("Poll ID is required").build();
         }
 
         QueryResponse response = pollRepository.getOptionsByPollId(pollId);
+        log.debug("Options fetched for pollId: {}: {}", pollId, response.items());
 
         return Response.ok(fromQueryResponse(response)).build();
     }
@@ -116,13 +135,18 @@ public class PollServiceImpl implements PollService {
     @Produces(MediaType.APPLICATION_JSON)
     @Override
     public Response createPoll(Map<String, Object> newPollData) {
+        log.info("Received request to create a new poll: {}", newPollData);
+
         if (!newPollData.containsKey("question") ||
                 !(newPollData.get("question") instanceof String question)) {
+            log.warn("Poll question is missing or invalid: {}", newPollData.get("question"));
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity("Poll question is required and should be a string").build();
         }
+
         if (!newPollData.containsKey("options") ||
                 !(newPollData.get("options") instanceof List)) {
+            log.warn("Poll options are missing or invalid: {}", newPollData.get("options"));
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity("Poll options are required and should be a list").build();
         }
@@ -131,19 +155,21 @@ public class PollServiceImpl implements PollService {
             Map<String, List<String>> pollDataToPut = new HashMap<>();
             newPollData.put(question, newPollData.get("options"));
 
+            log.debug("Attempting to create a new poll with data: {}", newPollData);
             if (pollRepository.createPoll(pollDataToPut)) {
+                log.info("Poll created successfully");
                 return Response.status(Response.Status.CREATED).entity("Poll created successfully").build();
             }
         } catch (DynamoDbException e) {
-            log.error("Error creating poll: {}", e.getMessage());
+            log.error("Error creating poll: {}", e.getMessage(), e);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
                     .entity("Error creating poll").build();
         }
 
+        log.error("Failed to create poll with data: {}", newPollData);
         return Response.status(Response.Status.BAD_REQUEST)
                 .entity("Error creating poll")
                 .build();
-
     }
 
     @GET
@@ -151,7 +177,10 @@ public class PollServiceImpl implements PollService {
     @Produces(MediaType.APPLICATION_JSON)
     @Override
     public Response getPollVotes(@QueryParam("pollId") String pollId) {
+        log.info("Received request to get votes for pollId: {}", pollId);
+
         QueryResponse response = pollRepository.getVotesByPollId(pollId);
+        log.debug("Votes fetched for pollId: {}: {}", pollId, response.items());
 
         List<Map<String, Object>> jsonResponse = response.items()
                 .stream()
@@ -166,5 +195,4 @@ public class PollServiceImpl implements PollService {
 
         return Response.ok(jsonResponse).build();
     }
-
 }
